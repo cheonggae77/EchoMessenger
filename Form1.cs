2c5769ae782092de2fcecb4b47a34e4786bdf321
SANGHUN CHOI <cheonggae77@gmail.com>
2026-03-19 18:05:57 +0900

    과제2 코딩과 테스트 완료

--- Form1.cs
+++ Form1.cs
@@ -2,16 +2,40 @@
 {
     public partial class Form1 : Form
     {
+        
         public Form1()
         {
             InitializeComponent();
+            // Make Enter trigger the send button when typing in the textbox
+            this.AcceptButton = btnSendButton;
+        }
+
+        private void TxtInput_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                // call the actual button control (fixed name)
+                btnSendButton.PerformClick();
+            }
         }
 
         private void btnSentButton_Click(object sender, EventArgs e)
         {
-            typed_msg = txtInput.Text;
-            libTextSpace.Items.Add(typed_msg);
-            txtInput.Clear();
+            string typed_msg = txtInput.Text;
+
+            if (string.IsNullOrWhiteSpace(typed_msg))
+            {
+                // 비어있으면 아무 동작도 하지 않거나 사용자에게 알림
+                // MessageBox.Show("메시지를 입력하세요.");
+                txtInput.Focus();
+            }
+            else
+            {
+                libTextSpace.Items.Add(typed_msg);
+                txtInput.Clear();
+                txtInput.Focus();
+
+            }
         }
     }
 }
